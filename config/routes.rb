98169d370b5205028a545d9b05ef35c862e4fbab
alicifes <635023574@qq.com>
2022-08-22 22:28:24 +0800

    add

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  post '/users', to: 'users#create'
-  get '/users/:id', to: 'users#show'
+  namespace :api do
+    namespace :v1 do
+      # /api/vi
+      resoureces :validation_codes, only: [:create]
+      resourece  :session, only: [:create, :destory]
+      resourece  :me, only: [:show]
+      resoureces :items
+      resoureces :tags
+    end
+  end
 end
