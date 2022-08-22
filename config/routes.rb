3409bb1885f93e4a4290f597adc5a8fde5e7a1a9
alicifes <635023574@qq.com>
2022-08-22 22:57:45 +0800

    创建Validation Controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,11 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       # /api/vi
-      resoureces :validation_codes, only: [:create]
-      resourece  :session, only: [:create, :destory]
-      resourece  :me, only: [:show]
-      resoureces :items
-      resoureces :tags
+      resources :validation_codes, only: [:create]
+      resource :session, only: [:create, :destroy]
+      resource :me, only: [:show]
+      resources:items
+      resources:tags
     end
   end
 end
